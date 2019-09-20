0b8c0c606113eee0786296e2e75798ca37876ff1
Rodrigo Torres <rtorres_1999@hotmail.com>
2019-09-20 19:05:02 -0300

    implementado envio por wpp

--- src/Library/WhatsAppChannel.cs
+++ src/Library/WhatsAppChannel.cs
@@ -4,7 +4,11 @@ namespace Library
     {
         public void Send(Message message)
         {
-            throw new System.NotImplementedException();
+            const string accountSid = "ACa8395e7223e7719988c09db8dfb68f0f";
+            const string authToken = "fdcaac12dc7e5b031908429ee402e7a4";
+
+            var whatsApp = new WhatsAppApi(accountSid, authToken);
+            whatsApp.Send(message.To, message.Text);
         }
     }
 }
