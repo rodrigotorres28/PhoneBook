c2496df4aedb535ce86c02644e95fc3fb77fcd96
Rodrigo Torres <rtorres_1999@hotmail.com>
2019-09-20 18:28:27 -0300

    Initial Commit

---
+++ src/Library/WhatsAppChannel.cs
@@ -0,0 +1,10 @@
+namespace Library
+{
+    public class WhatsAppChannel : IMessageChannel
+    {
+        public void Send(Message message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
